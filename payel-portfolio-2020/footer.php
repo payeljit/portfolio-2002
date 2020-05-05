c1719ef01bc346d9a0e69e5fd2152591e8ee94bb
Payle <payelbanerjee9@yahoo.com>
2020-05-05 19:56:44 +0100

    Footer

---
+++ payel-portfolio-2020/footer.php
@@ -0,0 +1,21 @@
+
+<footer class="row footer">
+    <div class="col-12">
+      &copy;<?php echo date('Y'); ?>
+      Payel Banerjee - Payel Banerjee Theme <?php echo date('Y'); ?>. <a href="#">My blog</a>.
+    </div>
+</footer>
+
+<?php wp_footer(); ?>
+</main>
+</div><!-- /Container-->
+
+</script><script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+<!-- scrollMagic -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/ScrollMagic.min.js"></script>
+<!-- GSAP Tweenmax -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.2.6/gsap.min.js"></script>
+
+</body>
+</html>
